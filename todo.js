1022ed1bc7c2bf9e5845c9dd72c8a88c819b268b
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-03-02 22:00:49 +0600

    made a simple todo list by using api

---
+++ todo.js
@@ -0,0 +1,24 @@
+const loadTodo = () => {
+  const url = "https://jsonplaceholder.typicode.com/users/1/todos";
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayTodos(data));
+};
+
+const displayTodos = (todos) => {
+  const todoContainer = document.getElementById("todo-container");
+  todoContainer.innerHTML = "";
+
+  todos.forEach((todo) => {
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <div class="todo-card">
+        <p>${todo.completed == true ? `<i class="fa-solid fa-square-check"></i>` : `<i class="fa-regular fa-square-check"></i>`}</p>
+        <h4>${todo.title}</h4>
+    </div>
+    `;
+    todoContainer.append(div);
+  });
+};
+
+loadTodo();
