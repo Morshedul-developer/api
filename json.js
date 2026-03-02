3ac898500e7964f127132ac2a5f5aaa0981a40cc
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-03-02 17:44:22 +0600

    explored how to fetch a lot of data

--- json.js
+++ json.js
@@ -3,3 +3,16 @@ const loadData = () => {
     .then(res => res.json())
     .then(data => console.log(data))   
 }
+
+const loadPost = () => {
+    const url = "https://jsonplaceholder.typicode.com/posts";
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>displayPost(data))
+}
+
+const displayPost = (posts) => {
+    posts.forEach(post => {
+        console.log(post);
+    });
+}
